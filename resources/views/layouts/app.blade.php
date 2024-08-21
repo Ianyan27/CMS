9b3883efc5e71113cecc37c14e3124b49f3f2824
Ianyan27 <gerome5252005@gmail.com>
2024-08-21 19:34:35 +0800

    Fix navbar height

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
             </div>
         </div>
         <div class="row my-4" style="">
-            <div id="side-bar" class="col-auto dashboard border-right-educ rounded-right" style="width:250px;padding:0;">
+            <div id="side-bar" class="col-auto dashboard border-right-educ rounded-right" style="width:250px;padding:0; min-height: 750px;">
                 <ul class="nav flex-column fonts w-100 mt-2">
                     {{-- <li class="nav-item">
                         <a class="nav-link {{ Route::currentRouteName() == 'dashboard' ? 'active-link' : '' }}"
