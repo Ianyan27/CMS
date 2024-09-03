83461b8b27a2e5a1603856fefab6d2405358092a
webstriix <webstriix@gmail.com>
2024-09-03 12:37:35 +0700

    prettier the code

--- resources/views/Sale_Agent_Page.blade.php
+++ resources/views/Sale_Agent_Page.blade.php
@@ -4,119 +4,121 @@
 @extends('layouts.Add_Sales-Agent_Modal')
 
 @section('content')
-@if (Auth::check() && Auth::user()->role == 'BUH')
-    @if (Session::has('success'))
-        <!-- Success Modal -->
-        <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header"
-                        style="background: linear-gradient(180deg, rgb(255, 180, 206) 0%, hsla(0, 0%, 100%, 1) 100%);
+    @if (Auth::check() && Auth::user()->role == 'BUH')
+        @if (Session::has('success'))
+            <!-- Success Modal -->
+            <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header"
+                            style="background: linear-gradient(180deg, rgb(255, 180, 206) 0%, hsla(0, 0%, 100%, 1) 100%);
                         border:none;border-top-left-radius: 0; border-top-right-radius: 0;">
-                        <h5 class="modal-title" id="successModalLabel" style="color: #91264c"><strong>Success</strong></h5>
-                    </div>
-                    <div class="modal-body" style="color: #91264c;border:none;">
-                        {{ Session::get('success') }}
-                    </div>
-                    <div class="modal-footer" style="border:none;">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal"
-                            style="background: #91264c; color:white;">OK</button>
+                            <h5 class="modal-title" id="successModalLabel" style="color: #91264c"><strong>Success</strong>
+                            </h5>
+                        </div>
+                        <div class="modal-body" style="color: #91264c;border:none;">
+                            {{ Session::get('success') }}
+                        </div>
+                        <div class="modal-footer" style="border:none;">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal"
+                                style="background: #91264c; color:white;">OK</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    @endif
-    <div class="container-max-height">
-        <div class="table-title d-flex justify-content-between align-items-center mb-3">
-            <div class="d-flex align-items-center">
-                <h2 style="margin: 0 0.5rem 0 0.25rem;" class="font-educ headings">Sales Agents</h2>
-                <button class="btn hover-action add-sales-agent-button" data-toggle="modal"
-                    data-target="#addSalesAgentModal" style="padding: 10px 12px;">
-                    <i class="fa-solid fa-square-plus"></i>
+        @endif
+        <div class="container-max-height">
+            <div class="table-title d-flex justify-content-between align-items-center mb-3">
+                <div class="d-flex align-items-center">
+                    <h2 style="margin: 0 0.5rem 0 0.25rem;" class="font-educ headings">Sales Agents</h2>
+                    <button class="btn hover-action add-sales-agent-button" data-toggle="modal"
+                        data-target="#addSalesAgentModal" style="padding: 10px 12px;">
+                        <i class="fa-solid fa-square-plus"></i>
                     </button>
-            </div>
-            <div class="d-flex align-items-center mr-3">
-                <div class="search-box d-flex align-items-center ml-3">
-                    <input type="search" class="form-control mr-1" placeholder="Search Name" id="search-name"
-                        aria-label="Search">
+                </div>
+                <div class="d-flex align-items-center mr-3">
+                    <div class="search-box d-flex align-items-center ml-3">
+                        <input type="search" class="form-control mr-1" placeholder="Search Name" id="search-name"
+                            aria-label="Search">
                         <button style="padding: 10px 12px;" class="btn hover-action" type="button" data-toggle="tooltip"
-                        title="Search">
-                        <i class="fa-solid fa-magnifying-glass"></i>
-                    </button>
+                            title="Search">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="table-container">
-            <table id="sales-agents-table" class="table table-hover mt-2">
-                <thead class="font-educ text-left">
-                    <tr>
-                        <th scope="col" id="name-header">Name
-                            <i class="ml-2 fa-sharp fa-solid fa-arrow-down-z-a" id="sortDown-agent"
-                                onclick="sortByColumn('agent', 'asc'); toggleSort('sortDown-agent', 'sortUp-agent')"></i>
-                            <i class="ml-2 fa-sharp fa-solid fa-arrow-up-a-z" id="sortUp-agent"
-                                onclick="sortByColumn('agent', 'desc'); toggleSort('sortUp-agent', 'sortDown-agent')"
-                                style="display: none;"></i>
-                        </th>
-                        <th scope="col">Hubspot ID</th>
-                        <th scope="col" id="country-header">Country
-                            <i class="ml-2 fa-sharp fa-solid fa-arrow-down-z-a" id="sortDown-country"
-                                onclick="sortByColumn('country', 'asc'); toggleSort('sortDown-country', 'sortUp-country')"></i>
-                            <i class="ml-2 fa-sharp fa-solid fa-arrow-up-a-z" id="sortUp-country"
-                                onclick="sortByColumn('country', 'desc'); toggleSort('sortUp-country', 'sortDown-country')"
-                                style="display: none;"></i>
-                        </th>
-                        <th scope="col" class="text-center">Total Assign Contacts</th>
-                        <th scope="col" class="text-center">Total Hubspot Sync</th>
-                        <th scope="col" class="text-center">Total In Progress</th>
-                        <th scope="col ">Action</th>
-                    </tr>
-                </thead>
-                <tbody class="text-left fonts">
-                    @foreach ($owner as $owners)
+            <div class="table-container">
+                <table id="sales-agents-table" class="table table-hover mt-2">
+                    <thead class="font-educ text-left">
                         <tr>
-                            <td>{{ $owners->owner_name }}</td>
-                            <td>{{ $owners->owner_hubspot_id }}</td>
-                            <td>
-                                @inject('countryCodeMapper', 'App\Services\CountryCodeMapper')
-
-                                @php
-                                    // Fetch the country code using the injected service
-                                    $countryCode = $countryCodeMapper->getCountryCode($owners['country']);
-                                @endphp
-
-                                @if ($countryCode)
-                                    <img src="{{ asset('flags/' . strtolower($countryCode) . '.svg') }}"
-                                        alt="{{ $owners['country'] }}" width="20" height="15">
-                                @else
-                                    <!-- Optional: Add a fallback image or text when the country code is not found -->
-                                    <span>No flag available</span>
-                                @endif
-                                {{ $owners['country'] }}
-                            </td>
-                            <td class="text-center">{{ $owners->total_assign_contacts }}</td>
-                            <td class="text-center">{{ $owners->total_hubspot_sync }}</td>
-                            <td class="text-center">{{ $owners->total_in_progress }}</td>
-                            <td>
-                                <a href="{{ route('owner#view-owner', $owners->owner_pid) }}" class="btn hover-action"
-                                    data-toggle="tooltip" title="View" style="padding: 10px 12px;">
-                                    <i class="fa-solid fa-eye"></i>
-                                </a>
-                                <!-- Delete button triggers the modal -->
-                                <a class="btn hover-action" style="padding: 10px 12px;" data-toggle="modal"
-                                    data-target="#deleteOwnerModal{{ $owners->owner_pid }}">
-                                    <i class="fa-solid fa-trash"></i>
-                                </a>
-                            </td>
+                            <th scope="col" id="name-header">Name
+                                <i class="ml-2 fa-sharp fa-solid fa-arrow-down-z-a" id="sortDown-agent"
+                                    onclick="sortByColumn('agent', 'asc'); toggleSort('sortDown-agent', 'sortUp-agent')"></i>
+                                <i class="ml-2 fa-sharp fa-solid fa-arrow-up-a-z" id="sortUp-agent"
+                                    onclick="sortByColumn('agent', 'desc'); toggleSort('sortUp-agent', 'sortDown-agent')"
+                                    style="display: none;"></i>
+                            </th>
+                            <th scope="col">Hubspot ID</th>
+                            <th scope="col" id="country-header">Country
+                                <i class="ml-2 fa-sharp fa-solid fa-arrow-down-z-a" id="sortDown-country"
+                                    onclick="sortByColumn('country', 'asc'); toggleSort('sortDown-country', 'sortUp-country')"></i>
+                                <i class="ml-2 fa-sharp fa-solid fa-arrow-up-a-z" id="sortUp-country"
+                                    onclick="sortByColumn('country', 'desc'); toggleSort('sortUp-country', 'sortDown-country')"
+                                    style="display: none;"></i>
+                            </th>
+                            <th scope="col" class="text-center">Total Assign Contacts</th>
+                            <th scope="col" class="text-center">Total Hubspot Sync</th>
+                            <th scope="col" class="text-center">Total In Progress</th>
+                            <th scope="col ">Action</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div aria-label="Page navigation example " class="paginationContainer">
-            <ul class="pagination justify-content-center">
-                <!-- Previous Button -->
-                <li class="page-item {{ $owner->onFirstPage() ? 'disabled' : '' }}">
-                        <a class="page-link font-educ" href="{{ $owner->previousPageUrl() }}" aria-label="Previous">&#60;</a>
+                    </thead>
+                    <tbody class="text-left fonts">
+                        @foreach ($owner as $owners)
+                            <tr>
+                                <td>{{ $owners->owner_name }}</td>
+                                <td>{{ $owners->owner_hubspot_id }}</td>
+                                <td>
+                                    @inject('countryCodeMapper', 'App\Services\CountryCodeMapper')
+
+                                    @php
+                                        // Fetch the country code using the injected service
+                                        $countryCode = $countryCodeMapper->getCountryCode($owners['country']);
+                                    @endphp
+
+                                    @if ($countryCode)
+                                        <img src="{{ asset('flags/' . strtolower($countryCode) . '.svg') }}"
+                                            alt="{{ $owners['country'] }}" width="20" height="15">
+                                    @else
+                                        <!-- Optional: Add a fallback image or text when the country code is not found -->
+                                        <span>No flag available</span>
+                                    @endif
+                                    {{ $owners['country'] }}
+                                </td>
+                                <td class="text-center">{{ $owners->total_assign_contacts }}</td>
+                                <td class="text-center">{{ $owners->total_hubspot_sync }}</td>
+                                <td class="text-center">{{ $owners->total_in_progress }}</td>
+                                <td>
+                                    <a href="{{ route('owner#view-owner', $owners->owner_pid) }}" class="btn hover-action"
+                                        data-toggle="tooltip" title="View" style="padding: 10px 12px;">
+                                        <i class="fa-solid fa-eye"></i>
+                                    </a>
+                                    <!-- Delete button triggers the modal -->
+                                    <a class="btn hover-action" style="padding: 10px 12px;" data-toggle="modal"
+                                        data-target="#deleteOwnerModal{{ $owners->owner_pid }}">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div aria-label="Page navigation example " class="paginationContainer">
+                <ul class="pagination justify-content-center">
+                    <!-- Previous Button -->
+                    <li class="page-item {{ $owner->onFirstPage() ? 'disabled' : '' }}">
+                        <a class="page-link font-educ" href="{{ $owner->previousPageUrl() }}"
+                            aria-label="Previous">&#60;</a>
                     </li>
                     <!-- First Page Button -->
                     @if ($owner->currentPage() > 3)
@@ -158,38 +160,38 @@
                 </ul>
             </div>
         </div>
-    </div>
-    @foreach ($owner as $owners)
-    <div class="modal fade" id="deleteOwnerModal{{ $owners->owner_pid }}" tabindex="-1" 
-        aria-labelledby="deleteOwnerModalLabel{{ $owners->owner_pid }}" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content text-center">
-                <div class="icon-container mx-auto">
-                    <i class="fa-solid fa-trash"></i>
-                </div>
-                <div class="modal-header border-0">
-                </div>
-                <div class="modal-body">
-                    <p>You are about to delete this Sales Agent</p>
-                    <p class="text-muted">This will delete your sales agent from your list.</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <form action="{{ route('owner#delete', $owners->owner_pid) }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form>
+        </div>
+        @foreach ($owner as $owners)
+            <div class="modal fade" id="deleteOwnerModal{{ $owners->owner_pid }}" tabindex="-1"
+                aria-labelledby="deleteOwnerModalLabel{{ $owners->owner_pid }}" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content text-center">
+                        <div class="icon-container mx-auto">
+                            <i class="fa-solid fa-trash"></i>
+                        </div>
+                        <div class="modal-header border-0">
+                        </div>
+                        <div class="modal-body">
+                            <p>You are about to delete this Sales Agent</p>
+                            <p class="text-muted">This will delete your sales agent from your list.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                            <form action="{{ route('owner#delete', $owners->owner_pid) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
+        @endforeach
+    @else
+        <div class="alert alert-danger text-center mt-5">
+            <strong>Access Denied!</strong> You do not have permission to view this page.
         </div>
-    </div>
-@endforeach
-@else
-<div class="alert alert-danger text-center mt-5">
-    <strong>Access Denied!</strong> You do not have permission to view this page.
-</div>
-@endif
+    @endif
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     @if (Session::has('success'))
         <script type="text/javascript">
